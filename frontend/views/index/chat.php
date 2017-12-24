f78263000ef4faebcbaa662954512043e1c74ddf
tsx <1130297686@qq.com>
2017-12-24 13:02:48 +0800

    sss

--- frontend/views/index/chat.php
+++ frontend/views/index/chat.php
@@ -4,16 +4,38 @@
 
 $this->title = 'TanShenXiao-聊天';
 ?>
-<div class="container">
+<style>
+    .footer{
+        display:none;
+    }
+    .table-bordered{
+        border-color:#337ab7;
+        height:50px;
+        line-height:50px;
+        font-size:larger;
+        color:#337ab7;
+        border-radius:20px;
+    }
+    .badge{
+        background-color:#337ab7;
+        margin:14px 0px;
+    }
+    .top-left{
+        width:60px;
+        height:50px;
+        text-align:center;
+        float:right;
+        line-height:50px;
+        vertical-align: middle;
+    }
+
+</style>
     <div class="row">
-        <div class="col-lg-2 table-bordered">a</div>
-        <div class="col-lg-10 table-bordered">b</div>
-
-
+        <div class="table-bordered">谭深潇<div class="top-left"><span class="badge">1</span></div></div>
+        <div class="table-bordered">含大一<div class="top-left"><span class="badge">1</span></div></div>
     </div>
-
-</div>
-<ul class="navbar navbar-default navbar-fixed-bottom">
-       <a class="navbar-brand">ssss</a>
-       <a class="navbar-brand">ssss</a>
+<ul class="nav nav-pills navbar-fixed-bottom nav-justified">
+    <li role="presentation" class="active"><a href="#">聊天</a></li>
+    <li role="presentation"><a href="#">好友</a></li>
+    <li role="presentation"><a href="#">动态</a></li>
 </ul>
