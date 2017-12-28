23d38bff5cc62b88bced6668afec068543352dd3
tsx <1130297686@qq.com>
2017-12-28 19:45:23 +0800

    s

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -19,12 +19,13 @@ class IndexController extends HomeController
         print_r($name->get("name"));
         exit;
        */
-        $redis=new \redis();
+        /*$redis=new \redis();
         $redis->connect("127.0.0.1");
         $redis->select(1);
         echo "<pre>";
         print_r($redis->lRange(45,0,100));
         exit;
+        */
 
 
         return $this->render('index');
